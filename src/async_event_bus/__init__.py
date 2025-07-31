6da5084914eaeac1eddf7b1215d4abf6dfe71a12
Half_nothing <Half_nothing@163.com>
2025-07-31 15:59:57 +0800

    feat: v0.3.0发布

--- src/async_event_bus/__init__.py
+++ src/async_event_bus/__init__.py
@@ -2,11 +2,13 @@ from .event import *
 from .event_bus import EventBus
 from .module import *
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __author__ = "Half_nothing"
 
 __ALL__ = [
     EnumEvent,
+    AbstractEvent,
+    EventType,
     EventCallback,
     SyncEventCallback,
     AsyncEventCallback,
